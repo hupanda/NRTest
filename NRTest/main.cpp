6ea95bee9c699e91ada57131468d9fa6b37dc718
Pan Hu <U0706290@gmail.com>
2013-07-28 21:15:48 +0800

    Initial Commit

---
+++ NRTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  NRTest
+//
+//  Created by Pan Hu on 28/7/13.
+//  Copyright (c) 2013 Pan Hu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
